378dc91e9d72bc4608015b04267d9db029e5da5b
Tibor Kotosz <tkotosz@inviqa.com>
2015-11-08 13:51:49 +0100

    Initial commit of the extenstion driver locator component

---
+++ src/Bex/Behat/ExtensionDriverLocator/DriverClassValidator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bex\Behat\ExtensionDriverLocator;
+
+use Bex\Behat\ExtensionDriverLocator\DriverInterface;
+
+
+class DriverClassValidator
+{
+    /**
+     * @var string
+     */
+    private $parent;
+
+    /**
+     * @param string $parent
+     */
+    public function __construct($parent = '')
+    {
+        $this->parent = $parent;
+    }
+
+    /**
+     * @param  string  $className
+     *
+     * @return boolean
+     */
+    public function isValidDriverClass($className)
+    {
+        if (!class_exists($className)) {
+            return false;
+        }
+
+        if (!is_subclass_of($className, DriverInterface::class)) {
+            return false;
+        }
+
+        if (!empty($this->parent) && !is_subclass_of($className, $this->parent)) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/Bex/Behat/ExtensionDriverLocator/DriverInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Bex\Behat\ExtensionDriverLocator;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+interface DriverInterface
+{
+    /**
+     * @param  ArrayNodeDefinition $builder
+     *
+     * @return void
+     */
+    public function configure(ArrayNodeDefinition $builder);
+
+    /**
+     * @param  ContainerBuilder $container
+     * @param  array            $config
+     *
+     * @return void
+     */
+    public function load(ContainerBuilder $container, array $config);
+}
